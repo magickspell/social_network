ca20cf302c84dd8db4f4014bcbbc7e64ae5dcfc9
Aleksandr Solomcheko <77538123+magickspell@users.noreply.github.com>
2021-05-19 20:52:23 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header.js';
+import Tech from './Tech.js';
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Tech />
+    </div>);
+}
+
+
+export default App;
+//export default App; this is a comment
---
+++ src/Tech.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Tech = () => {
+    return (
+        <div>
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Tech;
