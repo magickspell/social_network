ed34a80f02873385021999c10b36bb4ee0c16468
Aleksandr Solomcheko <77538123+magickspell@users.noreply.github.com>
2021-05-20 21:54:41 +0300

    Update Dialogs.jsx

--- src/components/Content/Dialogs/Dialogs.jsx
+++ src/components/Content/Dialogs/Dialogs.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import style from './Dialogs.module.css';
+import {NavLink} from 'react-router-dom';
 
 const Dialogs = (props) => {
     return (
         <div className={style.dialogs}>
             <div className={style.dialogsItems}>
                 <div className={style.dialog}>
-                    Shaman
+                    <NavLink to="/dialogs/1">Shaman</NavLink>
                 </div>
                 <div className={style.dialog}>
-                    Paladin
+                    <NavLink to="/dialogs/2">Paladin</NavLink>
                 </div>
                 <div className={style.dialog}>
-                    Mage
+                    <NavLink to="/dialogs/3"> Mage</NavLink>
                 </div>
                 <div className={style.dialog + ' ' + style.active}>
-                    Programmer
+                    <NavLink to={/dialogs/4}>Programmer</NavLink>
                 </div>
             </div>
             <div className={style.messages}>
                 <div className={style.message}>Hi</div>
-                <div className={style.message}>wazap</div>
+                <div className={style.message}>wat sap</div>
                 <div className={style.message}>wake up samurai</div>
             </div>
         </div>
