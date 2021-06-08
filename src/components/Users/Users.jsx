8b14bb0d22628bd729295a3ae43bb6271ad7fbfd
Aleksandr Solomcheko <77538123+magickspell@users.noreply.github.com>
2021-06-08 21:33:24 +0300

    lesson 54 componentDidMount
    lesson 54 componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,23 +5,11 @@ import userPhoto from '../../assets/images/user.jpg';
 
 class Users extends React.Component{
 
-    constructor(props) {
-        super(props);
-        alert('New object has been created')
-        if (this.props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                this.props.setUsers(response.data.items)
-            });
-        }
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items)
+    });
     }
-
-/*    getUsers = () => {
-        if (this.props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                this.props.setUsers(response.data.items)
-            });
-        }
-    }*/
 
     render() {
         return <div>
